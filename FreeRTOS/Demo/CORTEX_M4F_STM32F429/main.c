f8edecca861664e62e835d44cfab1234a931c50b
gtossk2 <gtossk2@hotmail.com>
2022-06-12 12:31:02 +0800

    FreeRTOS on STM32F429 and Demo for LED Blink Example
    STM32 Library - https://github.com/STMicroelectronics/STM32CubeF4.git
    Version - v1.27.0
    Files -
      Libraries/*
      STM32F429ZITx_FLASH.ld
      stm32f4xx_it.c
      stm32f4xx_it.h
      stm32f4xx_hal_conf.h
      system_stm32f4xx.c
    
    Signed-off-by: gtossk2 <gtossk2@hotmail.com>

---
+++ FreeRTOS/Demo/CORTEX_M4F_STM32F429/main.c
@@ -0,0 +1,128 @@
+#include "stm32f4xx_hal.h"
+#include "stm32f429i_discovery.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+
+#define mainTASK1_PRIORITY (tskIDLE_PRIORITY + 4)
+#define mainTASK2_PRIORITY (tskIDLE_PRIORITY + 3)
+
+static void SystemClock_Config(void);
+
+void _init(void) {};
+
+void vTask2(void* pvParameters)
+{
+  portTickType xLastWakeTime = xTaskGetTickCount();
+  for(;;)
+  {
+    BSP_LED_Toggle(LED4);
+    vTaskDelayUntil(&xLastWakeTime, (2000 / portTICK_RATE_MS));
+  }
+}
+
+void vTask1(void* pvParameters)
+{
+  for(;;)
+  {
+    BSP_LED_Toggle(LED3);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
+
+int main()
+{
+  HAL_Init();
+
+  BSP_LED_Init(LED3);
+  BSP_LED_Init(LED4);
+
+  SystemClock_Config();
+
+  xTaskCreate(vTask1, "vTask1", configMINIMAL_STACK_SIZE,
+              NULL, mainTASK1_PRIORITY, NULL);
+  xTaskCreate(vTask2, "vTask2", configMINIMAL_STACK_SIZE,
+              NULL, mainTASK2_PRIORITY, NULL);
+
+  vTaskStartScheduler();
+
+  for(;;);
+}
+
+static void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  
+  /* Enable Power Control clock */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  
+  /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 360;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+ 
+  /* Activate the Over-Drive mode */
+  HAL_PWREx_EnableOverDrive();
+ 
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+  clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+}
+
+void vApplicationTickHook(void) {
+}
+
+/* vApplicationMallocFailedHook() will only be called if
+   configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
+   function that will get called if a call to pvPortMalloc() fails.
+   pvPortMalloc() is called internally by the kernel whenever a task, queue,
+   timer or semaphore is created.  It is also called by various parts of the
+   demo application.  If heap_1.c or heap_2.c are used, then the size of the
+   heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
+   FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
+   to query the size of free heap space that remains (although it does not
+   provide information on how the remaining heap might be fragmented). */
+void vApplicationMallocFailedHook(void) {
+  taskDISABLE_INTERRUPTS();
+  for(;;);
+}
+
+/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
+   to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
+   task.  It is essential that code added to this hook function never attempts
+   to block in any way (for example, call xQueueReceive() with a block time
+   specified, or call vTaskDelay()).  If the application makes use of the
+   vTaskDelete() API function (as this demo application does) then it is also
+   important that vApplicationIdleHook() is permitted to return to its calling
+   function, because it is the responsibility of the idle task to clean up
+   memory allocated by the kernel to any task that has since been deleted. */
+void vApplicationIdleHook(void) {
+}
+
+void vApplicationStackOverflowHook(xTaskHandle pxTask, signed char *pcTaskName) {
+  (void) pcTaskName;
+  (void) pxTask;
+  /* Run time stack overflow checking is performed if
+     configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
+     function is called if a stack overflow is detected. */
+  taskDISABLE_INTERRUPTS();
+  for(;;);
+}
